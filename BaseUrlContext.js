77e6bb5383acc8b4aa5ce90da28d0de0be8112ec
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-12-29 19:19:55 +0530

    updated

--- BaseUrlContext.js
+++ BaseUrlContext.js
@@ -4,23 +4,23 @@ export const BaseUrlContext = createContext();
 export const UserDataContext = createContext(); // Create another context
 
 export const BaseUrlProvider = ({ children }) => {
-    const [baseUrl, setBaseUrl] = useState('https://acef-2405-201-c01d-402a-8531-44ce-b9e7-db7a.ngrok-free.app');
+    const [baseUrl, setBaseUrl] = useState('');
     const [userData, setUserData] = useState(''); // State for another context
 
-    // useEffect(() => {
-    //     const loadConfig = async () => {
-    //         try {
-    //             const response = await fetch('https://drive.google.com/uc?id=106BXNjo0tgpHl4PucP-NRA2sCF64FPCD');
-    //             const config = await response.json();
-    //             setBaseUrl(config.baseUrl);
-    //             console.log(config.baseUrl);
-    //         } catch (error) {
-    //             console.error('Error loading config:', error);
-    //         }
-    //     };
+    useEffect(() => {
+        const loadConfig = async () => {
+            try {
+                const response = await fetch('https://drive.google.com/uc?id=106BXNjo0tgpHl4PucP-NRA2sCF64FPCD');
+                const config = await response.json();
+                setBaseUrl(config.baseUrl);
+                console.log(config.baseUrl);
+            } catch (error) {
+                console.error('Error loading config:', error);
+            }
+        };
 
-    //     loadConfig();
-    // }, []);
+        loadConfig();
+    }, []);
 
     return (
         <BaseUrlContext.Provider value={baseUrl}>
